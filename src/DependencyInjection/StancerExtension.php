1840241f11c867127ce3bfe4975e734b9d0b3977
Sébastien Gamarde <sebastien.gamarde@mailoo.org>
2026-03-30 23:51:33 +0200

    feat: add Twig Hooks for gateway configuration form fields (create/update)

--- src/DependencyInjection/StancerExtension.php
+++ src/DependencyInjection/StancerExtension.php
@@ -15,6 +15,8 @@ final class StancerExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
+        $loader->load('twig_hooks/admin/payment_method/create.yaml');
+        $loader->load('twig_hooks/admin/payment_method/update.yaml');
     }
 
     public function getAlias(): string
