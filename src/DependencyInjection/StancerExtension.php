0b5578697dbee46fc13bafe0b79413e452c0d665
Sébastien Gamarde <sebastien.gamarde@mailoo.org>
2026-03-30 23:53:01 +0200

    fix: use PrependExtensionInterface for twig hooks instead of YAML loader

--- src/DependencyInjection/StancerExtension.php
+++ src/DependencyInjection/StancerExtension.php
@@ -7,16 +7,31 @@ namespace SpiderWeb\Sylius\StancerPlugin\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class StancerExtension extends Extension
+final class StancerExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
-        $loader->load('twig_hooks/admin/payment_method/create.yaml');
-        $loader->load('twig_hooks/admin/payment_method/update.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $credentialsTemplate = '@StancerPlugin/admin/payment_method/form/sections/gateway_configuration/credentials.html.twig';
+
+        $container->prependExtensionConfig('sylius_twig_hooks', [
+            'hooks' => [
+                'sylius_admin.payment_method.create.content.form.sections.gateway_configuration.stancer' => [
+                    'credentials' => ['template' => $credentialsTemplate, 'priority' => 100],
+                ],
+                'sylius_admin.payment_method.update.content.form.sections.gateway_configuration.stancer' => [
+                    'credentials' => ['template' => $credentialsTemplate, 'priority' => 100],
+                ],
+            ],
+        ]);
     }
 
     public function getAlias(): string
